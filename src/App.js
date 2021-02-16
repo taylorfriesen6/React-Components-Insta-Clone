64685c03c19abb3ef535928176a4d9acbf97bdc5
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-16 14:06:47 -0800

    clicking the heart increases the displayed likes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import React, {useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import Posts from './components/Posts/Posts.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
 import dummyData from './dummy-data.js';
 
 import './App.css';
@@ -17,6 +18,7 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+const [posts, setPosts] = useState(dummyData);
 
   const likePost = postId => {
     /*
@@ -30,11 +32,14 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map(p => p.id === postId ? {...p, likes: p.likes + 1} : p));
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts likePost={likePost} posts={posts}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
